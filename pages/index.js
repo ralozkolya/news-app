2e162be337f0dd1418d84f41ae4584e9e22b629b
Nikoloz Razmadze <raloz1@gmail.com>
2022-02-21 02:59:48 +0400

    Update homepage

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,14 @@ export default function Home() {
       <h1 className="mb-4">Welcome to the News app</h1>
       <p>This app offers the following functionality:</p>
       <ol>
-        <li>Query news from NewsApi.org <Link href="/import"><a>here</a></Link></li>
-        <li>Save chosen articles for later viewing <Link href="/import"><a>here</a></Link></li>
-        <li>See all the saved articles <Link href="/news"><a>here</a></Link></li>
+        <li>Query news from NewsApi.org <Link href="/import"><a>here</a></Link>;</li>
+        <li>Save chosen articles for later viewing <Link href="/import"><a>here</a></Link>;</li>
+        <li>See all the saved articles <Link href="/news"><a>here</a></Link>.</li>
       </ol>
+      <p>
+        This app is developed using <a href="https://nextjs.org/" target="_blank" rel="noreferrer">NextJS</a> framework,
+        and is backed by <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">MongoDB</a> NoSQL database.
+      </p>
     </>
   )
 }
