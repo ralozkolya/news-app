e6d04c457e3addb4d725ca58b0368465fe811095
Nikoloz Razmadze <raloz1@gmail.com>
2022-02-21 05:46:13 +0400

    Implement 'sel;ect all'

--- components/news-list.js
+++ components/news-list.js
@@ -1,5 +1,6 @@
 import moment from 'moment'
 import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { importSelector, setSelected } from '../redux/importSlice'
 
@@ -7,6 +8,8 @@ export default function NewsList({ news = [], selectable }) {
 
   const { selected } = useSelector(importSelector)
   const dispatch = useDispatch()
+
+  const [ allSelected, setAllSelected ] = useState(false)
 
   const handleSelect = article => {
     dispatch(
@@ -17,53 +20,77 @@ export default function NewsList({ news = [], selectable }) {
     )
   }
 
+  const handleSelectAll = ({ target: { checked } }) => {
+    news.forEach(article => {
+      dispatch(
+        setSelected({
+          id: article.id,
+          value: checked ? article : null
+        })
+      )
+    })
+    setAllSelected(checked)
+  }
+
   return (
     news.length ?
-      <div className="accordion mb-3" id="news-accordion">
+      <>
         {
-          news.map(article => (
-            <div className="accordion-item" key={ article.id }>
-              <h2 className="accordion-header d-flex">
-                {
-                  selectable &&
-                    <input
-                      className="mx-3"
-                      type="checkbox"
-                      onChange={ () => handleSelect(article) }
-                      checked={ article.id in selected } />
-                }
-                <button
-                  className="accordion-button collapsed"
-                  data-bs-toggle="collapse"
-                  data-bs-target={`#col-${ article.id }`}>
-                  {article.title}
-                </button>
-              </h2>
-              <div
-                className="accordion-collapse collapse"
-                id={`col-${ article.id }`}
-                data-bs-parent="#news-accordion">
-                <div className="accordion-body">
-                  <div className="row">
-                    <div className="col-12 col-md-4 d-flex align-items-center justify-content-center">
-                      <img className="mw-100" src={article.urlToImage} alt={article.title} />
-                    </div>
-                    <div className="col-12 col-md-8 d-flex flex-column">
-                      <div className="mb-3 flex-grow-1">{article.description}</div>
-                      <div className="text-muted small d-flex justify-content-between align-items-center">
-                        <a className="btn btn-primary" href={article.url} target="_blank" rel="noreferrer">
-                          Learn more
-                        </a>
-                        Date: {moment(article.publishedAt).format('YYYY-MM-DD HH:mm:ss')}
+          selectable && (
+            <div className="m-3">
+              <label>
+                <input className="me-3" type="checkbox" onChange={ handleSelectAll } />
+                { allSelected ? 'Deselect all' : 'Select all' }
+              </label>
+            </div>
+          )
+        }
+        <div className="accordion mb-3" id="news-accordion">
+          {
+            news.map(article => (
+              <div className="accordion-item" key={ article.id }>
+                <h2 className="accordion-header d-flex">
+                  {
+                    selectable &&
+                      <input
+                        className="mx-3"
+                        type="checkbox"
+                        onChange={ () => handleSelect(article) }
+                        checked={ article.id in selected } />
+                  }
+                  <button
+                    className="accordion-button collapsed"
+                    data-bs-toggle="collapse"
+                    data-bs-target={`#col-${ article.id }`}>
+                    {article.title}
+                  </button>
+                </h2>
+                <div
+                  className="accordion-collapse collapse"
+                  id={`col-${ article.id }`}
+                  data-bs-parent="#news-accordion">
+                  <div className="accordion-body">
+                    <div className="row">
+                      <div className="col-12 col-md-4 d-flex align-items-center justify-content-center">
+                        <img className="mw-100" src={article.urlToImage} alt={article.title} />
+                      </div>
+                      <div className="col-12 col-md-8 d-flex flex-column">
+                        <div className="mb-3 flex-grow-1">{article.description}</div>
+                        <div className="text-muted small d-flex justify-content-between align-items-center">
+                          <a className="btn btn-primary" href={article.url} target="_blank" rel="noreferrer">
+                            Learn more
+                          </a>
+                          Date: {moment(article.publishedAt).format('YYYY-MM-DD HH:mm:ss')}
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))
-        }
-      </div> :
+            ))
+          }
+        </div>
+      </> :
       <h5>No news have been found</h5>
   )
 }
